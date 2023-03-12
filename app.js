f32c8e1d636d2021e745567d20d1b4a4d255fda3
Jan Deinhard <jan.deinhard@gmail.com>
2023-03-12 11:04:12 +0100

    use import() instead of System.import()

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-(function (root, factory) {
-  if (typeof define === "function" && define.amd) {
-    define(["jquery", "underscore"], factory);
-  } else if (typeof exports === "object") {
-    module.exports = factory(require("jquery"), require("underscore"));
-  } else {
-    root.Requester = factory(root.$, root._);
-  }
-})(this, function ($, _) {
-  return {
-    hello: function () {
-      console.log("Hello World!");
-    },
-  };
-});
+export function hello() {
+  console.log('Hello');
+}
